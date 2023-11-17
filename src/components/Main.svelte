8c1c0f3cecdf105cc465271eafaf12a388c5a85b
Jorge Martín Capcha <jmcapcha@gmail.com>
2023-11-17 14:08:15 -0500

    UPDATE: Main component

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -1,1 +1,40 @@
-This is my main component.
+<script>
+</script>
+
+<main class="flex flex-col flex-1 p-4">
+  <section
+    id="introPage"
+    class="grid grid-cols-1 lg:grid-cols-2 gap-10 py-8 sm:py-14"
+  >
+    <div
+      class="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10"
+    >
+      <h2 class="font-semibold text-4xl sm:text-5xl md:text-6xl">
+        Hi! I'm <span class="robomono text-violet-400">Jorge</span> Capcha
+        <br />Full Stack <span class="robomono text-violet-400">Developer</span>
+      </h2>
+      <p class="text-base sm:text-lg md:text-xl">
+        My <span class="text-violet-400">favorite tech</span> includes JavaScript
+        or Typescript (Angular or SvelteKit), TailwindCSS, Ruby on Rails, Go + Gin
+        or Typescript + Nest.js & PostgreSQL!
+      </p>
+      <a
+        href="https://www.linkedin.com/in/jorgecapcha/"
+        target="_blank"
+        class="blueShadow mx-auto lg:mr-auto text-base sm:text-lg md:text-xl robomono relative overflow-hidden px-6 py-3 rounded-full bg-white text-slate-950"
+      >
+        <div
+          class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"
+        />
+        <h4 class="relative z-9">Get In Touch &rarr;</h4>
+      </a>
+    </div>
+    <div class="relative shadow-2xl grid place-items-center">
+      <img
+        src="/images/portfolio.jpg"
+        alt="profile"
+        class="object-cover z-[2] max-h-[70vh]"
+      />
+    </div>
+  </section>
+</main>
