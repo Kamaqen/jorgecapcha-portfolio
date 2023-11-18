08c6e21015751b26f70d33c64e1d5d1de75efac9
Jorge Martín Capcha <jmcapcha@gmail.com>
2023-11-18 00:55:30 -0500

    ADD: content to the about me section

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -13,6 +13,23 @@
     },
     { name: "Eatable", icon: "fa-solid fa-drumstick-bite" },
   ];
+  let benefits = [
+    {
+      name: "Tech Enthusiast",
+      description:
+        "As a Full Stack Developer, I'm on a perpetual journey of learning and exploration. From mastering frameworks to crafting seamless user experiences, I'm always up for a challenge. Embracing the flexibility of remote work, I bring my leadership experience to collaborative projects. Together, let's build something amazing, no matter where in the world you are.",
+    },
+    {
+      name: "Excellent Communicator",
+      description:
+        "Beyond the lines of code, I'm also a medical interpreter. I don't just speak languages; I craft seamless communication bridges. My ability to break down language barriers ensures that ideas flow effortlessly. I thrive on the challenge of conveying not just words, but the nuanced essence behind them, enhancing understanding and collaboration in both the tech world and beyond.",
+    },
+    {
+      name: "Harmony Seeker",
+      description:
+        "In the world of bits and bytes, I find balance in music. Whether I'm fine-tuning a melody or debugging a complex algorithm, the precision and creativity required in both domains are remarkably similar. This harmony-seeking mindset extends beyond the notes and lines of code, influencing my approach to problem-solving and fostering a balanced perspective in the dynamic world of technology.",
+    },
+  ];
 </script>
 
 <main class="flex flex-col flex-1 p-4">
@@ -98,6 +115,39 @@
           >, and it seamlessly interacts with a data API.
         </p>
       </Step>
+    </div>
+  </section>
+  <section
+    id="about"
+    class="py-20 pt-10 lg:pt-16 lg:py-32 flex flex-col gap-16 sm:gap-20 md:gap-24 relative"
+  >
+    <div
+      class="flex flex-col gap-2 text-center relative before:absolute before:top-0 before:left-0 before:w-2/3 before:h-1.5 before:bg-violet-700 after:absolute after:bottom-0 after:right-0 after:w-2/3 after:h-1.5 after:bg-violet-700 py-4"
+    >
+      <h6 class="text-lg sm:text-xl md:text-2xl">Want to know more?</h6>
+      <h3 class="font-semibold text-3xl sm:text-4xl md:text-5xl">
+        A bit <span class="robomono text-violet-400">about</span> me.
+      </h3>
+    </div>
+    <p class="ms-auto robomono font-semibold text-lg sm:text-xl md:text-2xl">
+      I am...
+    </p>
+    <div class="flex flex-col gap-20 w-full mx-auto max-w-[800px]">
+      {#each benefits as benefit, index}
+        <div class="flex gap-6 sm:gap-8">
+          <p
+            class="robomono text-4xl sm:text-5xl md:text-6xl text-slate-500 font-semibold"
+          >
+            0{index + 1}
+          </p>
+          <div class="flex flex-col gap-6 sm:gap-8">
+            <h3 class="text-2xl sm:text-3xl md:text-5xl">
+              {benefit.name}
+            </h3>
+            <p>{benefit.description}</p>
+          </div>
+        </div>
+      {/each}
     </div>
   </section>
 </main>
