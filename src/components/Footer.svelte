19d97af404918e4c2eed00b752bd06681508b4e1
Jorge Martín Capcha <jmcapcha@gmail.com>
2023-11-19 09:08:02 -0500

    ADD: Footer content

---
+++ src/components/Footer.svelte
@@ -0,0 +1,42 @@
+<footer
+  class="py-20 sm:py-32 bg-black border-t border-solid border-violet-950 flex flex-col gap-4 sm:gap-8 justify-center items-center"
+>
+  <p class="px-4 py-2 bg-white text-slate-950 font-medium">
+    Connect with me &darr;
+  </p>
+  <div class="flex flex-col gap-4 items-center justify-center">
+    <p><b class="pr-2">Email</b> jmcapcha@gmail.com</p>
+    <p>
+      <b class="pr-2">GitHub</b>
+      <a
+        href="https://github.com/Kamaqen"
+        class="text-violet-400"
+        target="_blank"
+      >
+        kamaqen <sup
+          ><span class="text-xs scale-75 pl-0.5">
+            <i
+              class="fa-solid fa-arrow-up-richt-from-square text-xs scale-[75%]"
+            />
+          </span></sup
+        >
+      </a>
+    </p>
+    <p>
+      <b class="pr-2">LinkedIn</b>
+      <a
+        href="www.linkedin.com/in/jorgecapcha"
+        class="text-violet-400"
+        target="_blank"
+      >
+        kamaqen <sup
+          ><span class="text-xs scale-75 pl-0.5">
+            <i
+              class="fa-solid fa-arrow-up-richt-from-square text-xs scale-[75%]"
+            />
+          </span></sup
+        >
+      </a>
+    </p>
+  </div>
+</footer>
