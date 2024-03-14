11a4ed17934403700f40a6482ea16a304e5ea61b
Jorge Martín Capcha <jmcapcha@gmail.com>
2024-03-14 16:12:51 -0500

    UPDATE: footer heading style

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,7 +1,7 @@
 <footer
   class="py-10 sm:py-20 bg-black border-t border-solid border-violet-950 flex flex-col gap-4 sm:gap-8 justify-center items-center"
 >
-  <p class="px-4 py-2 bg-white text-slate-950 font-medium">
+  <p class="mx-auto text-violet-400">
     Connect with me &darr;
   </p>
   <div class="flex flex-col gap-4 items-center justify-center">
@@ -34,13 +34,7 @@
         class="text-violet-400"
         target="_blank"
       >
-        https://www.linkedin.com/in/jorgecapcha <sup
-          ><span class="text-xs scale-75 pl-0.5">
-            <i
-              class="fa-solid fa-arrow-up-richt-from-square text-xs scale-[75%]"
-            />
-          </span></sup
-        >
+        https://www.linkedin.com/in/jorgecapcha 
       </a>
     </p>
   </div>
